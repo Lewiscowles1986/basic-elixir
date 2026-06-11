fa4c3f84c5badf3a6337f98047df18b765e7880f
Lewis Cowles <lewis+github@cowles.uk>
2026-06-11 00:00:14 +0000

    fix: config esbuild setup for devcontainer

--- config/config.exs
+++ config/config.exs
@@ -38,7 +38,13 @@ config :esbuild,
     args:
       ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
     cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
+    env: %{
+      "NODE_PATH" => [
+        System.get_env("MIX_DEPS_PATH") || Path.expand("../deps", __DIR__),
+        Mix.Project.build_path()
+      ],
+      "MIX_DEPS_PATH" => System.get_env("MIX_DEPS_PATH")
+    }
   ]
 
 # Configure tailwind (the version is required)
@@ -49,7 +55,8 @@ config :tailwind,
       --input=assets/css/app.css
       --output=priv/static/assets/css/app.css
     ),
-    cd: Path.expand("..", __DIR__)
+    cd: Path.expand("..", __DIR__),
+    env: %{"MIX_DEPS_PATH" => System.get_env("MIX_DEPS_PATH")}
   ]
 
 # Configure Elixir's Logger
